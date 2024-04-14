cf09a5a4b90b5815a651d3317d9457583881775a
felopy <feliksvoskanyan@gmail.com>
2024-04-14 14:39:43 +0400

    I added try-except blocks

--- tic_tac.py
+++ tic_tac.py
@@ -1,49 +1,58 @@
-#game Tic tac toe
-ml = [["_","_","_"],["_","_","_"],["_","_","_"]]
+#Tic-tac game
 
+ml = [["_","_","_"],["_","_","_"],["_","_","_"]]
+#Print board
 def print_board():
     for i in range(3):
         print(" ".join(ml[i]) + "\n")
 
+#In which case should X or Y win?
 def check():
-        for i in range(3):
-            if ml[i][0] == ml[i][1] == ml[i][2] != "_":
-                    return ml[i][0]
-            if ml[0][i] == ml[1][i] == ml[2][i] != "_":
-                    return ml[0][i]
+    for i in range(3):
+        if ml[i][0] == ml[i][1] == ml[i][2] != "_":
+            return ml[i][0]
+        if ml[0][i] == ml[1][i] == ml[2][i] != "_":
+            return ml[0][i]
 
-        if ml[0][2] == ml[1][1] == ml[2][0] != "_":
-            return ml[0][2]
-        if ml[0][0] == ml[1][1] == ml[2][2] != "_":
-            return ml[0][0]
-        return None
+    if ml[0][2] == ml[1][1] == ml[2][0] != "_":
+        return ml[0][2]
+    if ml[0][0] == ml[1][1] == ml[2][2] != "_":
+        return ml[0][0]
+    return None
 
-
+#The logic of the game
 def logic_game():
     count = 0
     while True:
         variant = input("Enter X or O: ")
-        if variant == "X":
-            dig = int(input("(X),Enter 1-9: "))
-            row = (dig - 1) // 3
-            col = (dig - 1) % 3
+        try:
+            if variant == "X":
+                dig = int(input("(X),Enter 1-9: "))
+                row = (dig - 1) // 3
+                col = (dig - 1) % 3
 
-            if ml[row][col] == "_":
-                ml[row][col] = variant
-                print_board()
-        if variant == "O":
-            variant= "O"
-            gig = int(input("(O),Enter 1-9: "))
-            row = (gig - 1) // 3
-            col = (gig - 1) % 3
+                if ml[row][col] == "_":
+                    ml[row][col] = variant
+                    print_board()
+            elif variant == "O":
+                gig = int(input("(O),Enter 1-9: "))
+                row = (gig - 1) // 3
+                col = (gig - 1) % 3
 
-            if ml[row][col] == "_":
-                ml[row][col] = variant
-                print_board()
+                if ml[row][col] == "_":
+                    ml[row][col] = variant
+                    print_board()
+            else:
+                print("Invalid input. Please enter X or O.")
+                continue
+        except (ValueError, IndexError):
+            print("Invalid input. Please enter a number between 1 and 9.")
+            continue
+
         if check():
-            return f"player {check()} win "
+            return f"Player {check()} wins!"
         elif all([cell != '_' for row in ml for cell in row]):
-            return "Its a draw"
+            return "It's a draw"
 
 print(logic_game())
 
